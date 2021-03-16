3757d8232c3ecda0c0c2b2d1166bd5c7c28b31f0
jamesborn89 <jamesborn89@gmail.com>
2021-03-16 13:07:31 +0400

    banner-about

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -5,21 +5,26 @@
     const burger = document.querySelector('.header__top-burger');
     const nemuClose = document.querySelector('.header__bottom-close');
     const menu = document.querySelector('.header__bottom');
+    const body = document.getElementsByTagName('body')[0];
     
     burger.addEventListener('click', () => {
         menu.classList.add('open');
+        body.classList.add('oh');
     });
 
     nemuClose.addEventListener('click', () => {
         menu.classList.remove('open');
+        body.classList.remove('oh');
     });
 
     window.addEventListener('click', (e) => {
 	    let target = e.target;
-            if(target.closest('.header__bottom-inner') && !target.closest('.header__bottom-close')) {
-                e.stopPropagation();
-            } else if (target.closest('.header__bottom-wrapper'))
+        
+        if(target.closest('.header__bottom-inner') && !target.closest('.header__bottom-close')) {
+            e.stopPropagation();
+        } else if (target.closest('.header__bottom-wrapper'))
             menu.classList.remove('open');
+            body.classList.remove('oh');
 	});
 
     // berger-menu END
