49dd1095c53f194df1878e97b1bcc0fd9ea7f4a4
jamesborn89 <jamesborn89@gmail.com>
2021-03-19 09:49:02 +0400

    fix

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -67,21 +67,72 @@
     });
 
 })();
-    // price END
+// price END
 
-    // social-menu START
+// social-menu START
 
 ; (function () {
 
-        const socialMenu = document.querySelector('.social-menu__social');
-        const socialMenuBtn = document.querySelector('.social-menu__btn');
+    const socialMenu = document.querySelector('.social-menu__social');
+    const socialMenuBtn = document.querySelector('.social-menu__btn');
 
-        socialMenuBtn.addEventListener('click', () => {
-            socialMenuBtn.classList.toggle('open');
-            socialMenu.classList.toggle('open');
-        });
+    socialMenuBtn.addEventListener('click', () => {
+        socialMenuBtn.classList.toggle('open');
+        socialMenu.classList.toggle('open');
+    });
 
 })();
 
-    // social-menu END
+// social-menu END
+
+// contacts START
+
+; (function () {
+
+    if (document.getElementById('map')) {
+
+        ymaps.ready(init);
+
+        let myMap;
+
+        function init() {
+
+            myMap = new ymaps.Map('map', {
+                center: [56.817656, 53.211194],
+                zoom: 16,
+                controls: ['zoomControl']
+            });
+
+            let myGeoObjects = [];
+
+            myMap.behaviors.disable('scrollZoom');
+            myMap.behaviors.enable('drag');
+
+            myGeoObjects = new ymaps.Placemark([56.817656, 53.211194], {
+                balloonContentBody: 'Частный наркологический центр',
+            }, {
+                iconLayout: 'default#image',
+                // Путь до нашей картинки
+                iconImageHref: 'img/logo.png',
+                // Размер по ширине и высоте
+                iconImageSize: [40, 47],
+                // Смещение левого верхнего угла иконки относительно
+                // её «ножки» (точки привязки).
+                iconImageOffset: [-40, -60]
+            });
+
+            let clusterer = new ymaps.Clusterer({
+                clusterDisableClickZoom: false,
+                clusterOpenBalloonOnClick: false,
+            });
+
+            clusterer.add(myGeoObjects);
+            myMap.geoObjects.add(clusterer);
+        }
+
+    }
+
+})();
+
+    // contacts END
 
