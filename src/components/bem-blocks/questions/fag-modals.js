b4e8824a26269dc49a4dec026bc295393d04059c
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2021-03-19 13:04:59 +0300

    Added js for faq-modals

---
+++ src/components/bem-blocks/questions/fag-modals.js
@@ -0,0 +1,27 @@
+;(function () {
+
+    const questions = document.querySelectorAll('.questions ul li');
+    const modalsWrap = document.querySelectorAll('.questions-modal-wrapper');
+    const modals = document.querySelectorAll('.questions-modal');
+    const close = document.querySelectorAll('.questions__close');
+    const body = document.getElementsByTagName('body')[0];
+
+    questions.forEach( (question, i) => {
+        question.addEventListener('click', () => {
+            modals[i].classList.add('questions-modal--open');
+            modalsWrap[i].classList.add('questions-modal-wrapper--open');
+            body.classList.add('oh');
+            body.style.marginRight = `${17}px`;
+        });
+    });
+
+    close.forEach( (close, i) => {
+        close.addEventListener('click', () => {
+            modals[i].classList.remove('questions-modal--open');
+            modalsWrap[i].classList.remove('questions-modal-wrapper--open');
+            body.classList.remove('oh');
+            body.style.marginRight = `${0}px`;
+        });
+    });
+    
+})();
