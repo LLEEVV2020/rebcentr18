d78944e80eafc4a27948d618b5b30c9fd11b7a5a
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2021-03-20 12:50:32 +0300

    Added js for modals

--- src/components/bem-blocks/questions/fag-modals.js
+++ src/components/bem-blocks/questions/fag-modals.js
@@ -1,25 +1,59 @@
 ;(function () {
+    
+    if (document.querySelector('.questions')) {
+        const questions = document.querySelectorAll('.questions ul li');
+        const body = document.getElementsByTagName('body')[0];
+        let modal = '';
+        let div = document.createElement('div');
+            div.classList.add('current-modal');
 
-    const questions = document.querySelectorAll('.questions ul li');
-    const modalsWrap = document.querySelectorAll('.questions-modal-wrapper');
-    const modals = document.querySelectorAll('.questions-modal');
-    const close = document.querySelectorAll('.questions__close');
-    const body = document.getElementsByTagName('body')[0];
+        questions.forEach(question => {
+            question.addEventListener('click', () => {
+                let title = question.children[0].textContent;
+                let html = question.children[2].outerHTML;
+                simpleModalCreate(title, html);
+            });
+        });
 
-    questions.forEach( (question, i) => {
-        question.addEventListener('click', () => {
-            modals[i].classList.add('questions-modal--open');
-            modalsWrap[i].classList.add('questions-modal-wrapper--open');
+        function simpleModalCreate(title, html) {
+            modal += `
+                <div class="overlay overlay--active">
+                    <div class="questions__close"></div>
+                    <div class="questions-modal">
+                        <h4>${title}</h4>
+                        ${html}
+                    </div>
+                 </div>
+            `;
+            div.innerHTML = modal;
+            body.appendChild(div);
             body.classList.add('oh');
-        });
-    });
+        }
 
-    close.forEach( (close, i) => {
-        close.addEventListener('click', () => {
-            modals[i].classList.remove('questions-modal--open');
-            modalsWrap[i].classList.remove('questions-modal-wrapper--open');
-            body.classList.remove('oh');
+        function closeModal() {
+            modal = '';
+            div.innerHTML = modal;
+            body.removeChild(div);
+            body.classList.remove('oh')
+        }
+    
+        window.addEventListener('click', (e) => {
+            if (e.target.classList.contains('questions__close')) {
+                document.querySelector('.questions-modal').classList.add('questions-modal--close');
+                setTimeout(() => {
+                    closeModal();
+                }, 400);
+            }
+
+            if (e.target.closest('.questions-modal') && !e.target.closest('.questions__close')) {
+                e.stopPropagation();
+            } else if (e.target.closest('.overlay')) {
+                document.querySelector('.questions-modal').classList.add('questions-modal--close');
+                setTimeout(() => {
+                    closeModal();
+                }, 400);
+            }
         });
-    });
+    }
     
 })();
